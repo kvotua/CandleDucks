14a3000fb0176a8466af7f855ae5b650c2c9b904
abc <bogdanseredin1@gmail.com>
2024-08-12 20:55:19 +0200

    Механика перетаскивания свечей

---
+++ Assets/Scripts/UI/CandlesInteractScripts/CandleManager.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CandleManager : MonoBehaviour
+{
+    private CandleInfo currentCandle;
+    public static Action<CandleInfo> OnCandlesSceneLoad;
+
+    private void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+        CandlesSlider.OnCandleIsSelected += SelectCandle;
+    }
+
+    public void SelectCandle(CandleInfo candle)
+    {
+        currentCandle = candle;
+        SceneManager.LoadScene(1);
+        Invoke("SendCandleIsSelectedActivator", 0.5f);
+    }
+
+    private void SendCandleIsSelectedActivator() => SendCandleIsSelected(currentCandle);
+    public static void SendCandleIsSelected(CandleInfo candleInfo)//
+    {
+        OnCandlesSceneLoad.Invoke(candleInfo);
+    }
+}
---
+++ Assets/Scripts/UI/CandlesInteractScripts/DataBaseCandles.cs
@@ -0,0 +1,53 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DataBaseCandles : MonoBehaviour
+{
+    [SerializeField] private List<CandleInfo> candles;
+    [SerializeField] private Transform candleContainer;
+    [SerializeField] private GameObject candleCell;
+
+    private void Awake()
+    {
+        DragCandle.OnCandleIsActivate += AddFullCandle;
+    }
+    private void Start()
+    {
+        LoadAllCandlesFromDataBase();
+    }
+
+    private void OnDestroy()
+    {
+        DragCandle.OnCandleIsActivate -= AddFullCandle;
+    }
+    private void LoadAllCandlesFromDataBase()
+    {
+        //создать ещё класс с айди и оставшемся временем горения свечки
+        //загрузка всех свечей с какой либо бд
+        AddCandlesToScreen();
+    }
+
+    private void AddCandlesToScreen()
+    {
+        foreach (var candle in candles)
+        {
+            CandleContainer curCandle = Instantiate(candleCell, candleContainer).GetComponent<CandleContainer>();
+            curCandle.SetupCandle(candle);
+        }
+    }
+
+    public void AddFullCandle(CandleInfo candle)
+    {
+        CandleInfo curCandle = candle;
+        curCandle.currentBurnTime = curCandle.burnTime;
+        Debug.Log(curCandle.currentBurnTime);
+        candles.Add(curCandle);
+        CandleContainer curCandleContainer = Instantiate(candleCell, candleContainer).GetComponent<CandleContainer>();
+        curCandleContainer.SetupCandle(candle);
+    }
+
+    public void SaveCandlesData()
+    {
+
+    }
+}
